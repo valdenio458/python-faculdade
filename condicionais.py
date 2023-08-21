92c202c2b9609923c1b3680bc0efe970f9c37a0d
valdenio458 <valdeniomartinho@gmail.com>
2023-08-21 20:22:45 -0300

    condici

---
+++ condicionais.py
@@ -0,0 +1,10 @@
+codigo_compra = 3225
+
+if codigo_compra == 3222: 
+    print("Compra à vista")
+elif codigo_compra == 3223: 
+    print("Compra a prazo no boleto")
+elif codigo_compra == 3224: 
+    print("Compra a prazo no cartão")
+else:
+    print("Código não cadastrado")
