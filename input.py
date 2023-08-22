eef8ed973f8ad6ba23e2f12a7f7f4d573c53306a
valdenio458 <valdeniomartinho@gmail.com>
2023-08-22 11:18:36 -0300

    Input

---
+++ input.py
@@ -0,0 +1,9 @@
+nome = input("Digite seu nome: ")
+# Usando formatadores de caracteres
+print("Olá, %s!" %(nome))
+
+# Usando a função format
+print("Olá, {}!".format(nome))
+
+# Usando string formatada
+print(f"Olá, {nome}!")
