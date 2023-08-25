f2829795dd3ea5a90aa17393a21ede2baea9da9c
valdenio458 <valdeniomartinho@gmail.com>
2023-08-25 19:12:02 -0300

    list_comprehension

--- TA2/list_comprehension.py
+++ TA2/list_comprehension.py
@@ -5,3 +5,11 @@ print("Antes da list_comprehension = ", linguagens)
 linguagens = [item.upper() for item in linguagens]
 
 print("Após a list_comprehension = ", linguagens)
+
+
+
+# Outro exemplo com números:
+
+# list = [2, 4, 6, 8]
+# list = [5 * item for item in list]
+# print(list) # [10, 20, 30, 40]
