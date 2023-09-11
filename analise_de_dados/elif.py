25b36577f9462f203a47a63ce3b4f968242fb623
valdenio458 <valdeniomartinho@gmail.com>
2023-09-11 17:11:13 -0300

    elif

---
+++ analise_de_dados/elif.py
@@ -0,0 +1,10 @@
+codigo_compra = 5111
+
+if codigo_compra == 5222:
+    print("Compra à vista.")
+elif codigo_compra == 5333:
+    print("Compra à prazo no boleto.")
+elif codigo_compra == 5444:
+    print("Compra à prazo no cartão.")
+else:
+    print("Código não cadastrado.")
