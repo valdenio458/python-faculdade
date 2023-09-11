a02b4ac653e8bfa8bf4e7998c0ac440dd7e1df6d
valdenio458 <valdeniomartinho@gmail.com>
2023-09-11 19:37:22 -0300

    move arquivos para outro diretório

---
+++ analise_de_dados/intro/break.py
@@ -0,0 +1,6 @@
+disciplina = "Linguagem de programação"
+for c in disciplina:
+    if c == 'a':
+        break
+    else:
+        print(c)
---
+++ analise_de_dados/intro/variaveis.py
@@ -0,0 +1,9 @@
+x = 10
+nome = 'aluno'
+nota = 8.75
+fez_inscriçao = True
+
+print(type(x))
+print(type(nome))
+print(type(nota))
+print(type(fez_inscriçao))
