eac4d8b5a5c92865392789dc31c5dc22654b1e3e
valdenio458 <valdeniomartinho@gmail.com>
2023-09-13 17:13:06 -0300

    tuplas

---
+++ analise_de_dados/estrutura-de-dados/tuple.py
@@ -0,0 +1,10 @@
+# Em Python, as tuplas podem ser construídas de três maneiras:
+
+# Usando um par de parênteses para denotar uma tupla vazia: tupla1 = ()
+# Usando um par de parênteses e elementos separados por vírgulas: tupla2 = ('a', 'b', 'c')
+# Usando o construtor de tipo: tuple()
+
+vogais = ('a', 'e', 'i', 'o', 'u')
+
+for x in enumerate (vogais): 
+  print(x)
