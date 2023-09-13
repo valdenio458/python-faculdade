3cda8662257f65127f1c6fc1f01347f7bde01333
valdenio458 <valdeniomartinho@gmail.com>
2023-09-13 14:52:35 -0300

    função para abrir arquivos

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-my_list = [2, 'True', 'Joana', 13.5]
-my_list.append('Rita')
-print(my_list) #[2, 'True', 'Joana', 13.5, 'Rita'] 
+# my_list = [2, 'True', 'Joana', 13.5]
+# my_list.append('Rita')
+# print(my_list) #[2, 'True', 'Joana', 13.5, 'Rita'] 
+
+# my_dict = {'name': 'Joana', 'idade': 33}
+# print(my_dict['name'][2:])
+# my_dict['name'] = 'Carla'
+# print(my_dict.values())
+
+my_file = open('./analise_de_dados/intro/lambda.py')
+print(my_file.read())
