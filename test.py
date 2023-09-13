24442a4692be92d7b640fd5584bce068f59d44b0
valdenio458 <valdeniomartinho@gmail.com>
2023-09-13 17:13:55 -0300

    testes

--- test.py
+++ test.py
@@ -7,5 +7,17 @@
 # my_dict['name'] = 'Carla'
 # print(my_dict.values())
 
-my_file = open('./analise_de_dados/intro/lambda.py')
-print(my_file.read())
+# open()
+# my_file = open('./analise_de_dados/intro/lambda.py')
+# print(my_file.read())
+
+# x = set()
+# x.add(1) # {1}
+# x.add(2) # {1,2}
+# x.add(3) # {1,2,3}
+# x.add(2) # {1,2,3}
+# print(x) # {1,2,3}
+
+list = [2, 22, 22, 4, 5, 8, 8, 9, 10, 10]
+set_list = set(list)
+print(set_list) # {2, 4, 5, 8, 9, 10, 22}
