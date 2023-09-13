ecda6297837bf6444ded5a11bc07c29cf969f1d4
valdenio458 <valdeniomartinho@gmail.com>
2023-09-13 17:13:22 -0300

    sets

---
+++ analise_de_dados/estrutura-de-dados/set.py
@@ -0,0 +1,7 @@
+# Em Python, os objetos do tipo set podem ser construídos destas maneiras:
+
+# Usando um par de chaves e elementos separados por vírgulas: set1 = {'a', 'b', 'c'}
+# Usando o construtor de tipo: set(iterable)
+
+num = {2, 2, 3, 5, 8, 9, 12, 12, 33}
+print(num)
