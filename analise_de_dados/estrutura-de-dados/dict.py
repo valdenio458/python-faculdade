90140e6608aa42403bebec864c60e91747f6d0e2
valdenio458 <valdeniomartinho@gmail.com>
2023-09-13 17:13:14 -0300

    dicionários

---
+++ analise_de_dados/estrutura-de-dados/dict.py
@@ -0,0 +1,11 @@
+# Podemos construir dicionários em Python das seguintes maneiras:
+
+# Usando um par de chaves para denotar um dict vazio: dicionario1 = {}
+# Usando um par de elementos na forma, chave : valor separados por vírgulas: dicionario2 = {'one': 1, 'two': 2, 'three': 3}
+# Usando o construtor de tipo: dict()
+
+data = {'nome': 'João', 'idade': 30}
+print(data['idade'])
+print(data.keys())
+print(data.values())
+print(data.items())
