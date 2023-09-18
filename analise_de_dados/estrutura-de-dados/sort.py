d28d568fb029e9069fdcd7f8a6bc1c4c42337994
valdenio458 <valdeniomartinho@gmail.com>
2023-09-18 13:57:04 -0300

    métodos sort e sorted

---
+++ analise_de_dados/estrutura-de-dados/sort.py
@@ -0,0 +1,9 @@
+lista = [10, 4, 1, 15, -3]
+
+lista_ordenada1 = sorted(lista)
+lista_ordenada2 = lista.sort()
+
+print('lista = ', lista, '\n')
+print('lista_ordenada1 = ', lista_ordenada1)
+print('lista_ordenada2 = ', lista_ordenada2)
+print('lista = ', lista)
