1e8d15b9669482e7569a7ccca8e118a116337905
valdenio458 <valdeniomartinho@gmail.com>
2023-09-20 15:41:10 -0300

    desafio 1 corrigido

--- analise_de_dados/analise/desafio1.py
+++ analise_de_dados/analise/desafio1.py
@@ -10,9 +10,10 @@ hora_extracao = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 bsp_texto = BeautifulSoup(texto_string, 'html.parser')
 lista_noticias = bsp_texto.find_all('div', attrs={'class':'feed-post-body'})
 print("Quantidade de manchetes = ", len(lista_noticias))
-lista_noticias[0].contents
+# print(lista_noticias[0].contents)
 
-lista_noticias[0].contents[1].text.replace('"',"")
+print(lista_noticias[0].contents[1].text.replace('"',""))
 
-lista_noticias[0].find('a').get('href')
+print(lista_noticias[0].find('a').get('href'))
 
+descricao = lista_noticias[0].contents[2].text
