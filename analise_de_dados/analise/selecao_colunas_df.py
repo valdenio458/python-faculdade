75bb2098755cfc3db4adb2e80543644f6c83ca2f
valdenio458 <valdeniomartinho@gmail.com>
2023-09-20 11:46:57 -0300

    seleção de colunas em dataframes

---
+++ analise_de_dados/analise/selecao_colunas_df.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+data = {'Nome': ['Alice', 'Bob', 'Charlie'],
+        'Idade': [25, 30, 35],
+        'Cidade': ['Nova York', 'Los Angeles', 'Chicago']}
+
+
+df = pd.DataFrame(data)
+
+print(type(df['Nome']))
+# print(df.Nome)
